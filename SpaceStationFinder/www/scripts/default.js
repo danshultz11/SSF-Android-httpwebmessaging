c5905aca0c50d64ef59444f543da53e62619a457
danclarivoy <dan@clarivoy.com>
2021-05-25 11:50:32 -0400

    first time pushing httpwebmessaging

---
+++ SpaceStationFinder/www/scripts/default.js
@@ -0,0 +1,370 @@
+﻿var androidNs = (function () {
+    "use strict";
+var geocoder;
+
+    document.addEventListener('DOMContentLoaded', function () { onDeviceReady(); });
+
+function onDeviceReady() {
+    
+    while (google == null) {
+        if (google != null) {
+            break;
+        }
+    }
+
+    geocoder = new google.maps.Geocoder();
+
+    var test = "test";
+    $("#txtAddressSplash").focus();
+
+    $("#btnGetLatLon").click(function () { getLatLonViaGoogle(); });
+    $("#btnUsePhoneLatLon").click(function () {
+        getSpaceStationPassInfoWithPhoneCoordinates();
+    });
+
+    setLocalizedText(); //from localization.js
+    $("#lblClearLocations").click(function () { removeStoredLocations(); });
+
+    getPreviousLocationsFromLocalStorage();
+
+    $("#waitAnim").attr("src", "images/Sputnik02.png");
+    $("#waitAnim").attr("src", "images/Sputnik03.png");
+    $("#waitAnim").attr("src", "images/Sputnik04.png");
+    $("#friendlyMessage").click(function () { $("#friendlyMessage").text(""); });
+
+    window.addEventListener('message', function (event) {
+        alert('win heard event: ');
+    });
+    document.addEventListener('message', function (event) {
+        alert('doc heard event: ');
+    });
+
+    navigator.geolocation.getCurrentPosition(onGeolocationSuccess, getCurrentPositionError,
+        {
+            maximumAge: 90000, timeout: 10000, enableHighAccuracy: false
+        }
+    );
+};
+
+
+var SPACE_STATION_ID = "25544";
+var PM_AM = "PM";
+
+function populateIFrame(qs) {
+    var page = "results.html";
+    if (qs.indexOf("page=main") > -1) {
+        page = "index.html";
+    }
+    //Testing remove:
+
+    page = "resultsWithReminder.html";
+    var fullUrl = "http://spacestationfinder.com/" + page + qs;
+    document.location = fullUrl + '&test=' + Math.random();
+   
+    
+}
+
+function getLatLonViaGoogle(desiredCity) {
+    var address = desiredCity == null ? $("#txtAddress").val() : desiredCity;
+    $("#locationText").text("Getting Location...");
+
+    if (geocoder) {
+        geocoder.geocode({ 'address': address }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                $("#locationText").text(results[0].formatted_address);
+                $("#lat").val(results[0].geometry.location.lat());
+                $("#lng").val(results[0].geometry.location.lng());
+                addToPreviousLocations(address);
+                getSpaceStationPassInfo();
+            }
+            else {
+                $("#locationText").text("Error: " + status);
+                $("#friendlyMessage").text("Awww, SNAP! - " + friendlyGeocodeErrorMessage(status));
+            }
+        });
+
+    }
+}
+
+function getSpaceStationPassInfoWithPhoneCoordinates() {
+    getSpaceStationPassInfo();
+}
+
+function getSpaceStationPassInfo() {
+    var satId = SPACE_STATION_ID;
+    var lat = $("#lat").val();
+    var lng = $("#lng").val();
+    getCountryInfo();
+}
+
+function getCountryInfo() {
+    $("#btnUsePhoneLatLon").attr("disabled", true);
+    $("#btnGetLatLon").attr("disabled", true);
+    var geocoder = new google.maps.Geocoder();
+    var lat = parseFloat($("#lat").val());
+    var lng = parseFloat($("#lng").val());
+    var latlng = new google.maps.LatLng(lat, lng);
+    if (!isNaN(lat) && !isNaN(lng)) {
+        toggleWaitAnim(true);
+        $("#specialMessage").text("Getting ISS Data...");
+        geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                if (results[0]) {
+                    var cityStateCountryDelimitedString = getCityStateCountryFromAddressComponents(results[0].address_components);
+                    var cityStateCountryArray = cityStateCountryDelimitedString.split("|");
+                    var city = cityStateCountryArray[0];
+                    var state = cityStateCountryArray[1];
+                    var country = cityStateCountryArray[2];
+                    city = replaceDiacritics(city);
+                    state = replaceDiacritics(state);
+                    addToPreviousLocations(city);
+                    populateIFrame("?loc=" + city + "%20" + state + "&src=and");
+                }
+            } else {
+                populateIFrame("?page=main&src=and");
+            }
+        }
+          )
+    } else {
+        populateIFrame("?page=main&src=and");
+    }
+    $("#btnUsePhoneLatLon").removeAttr("disabled");
+    $("#btnGetLatLon").removeAttr("disabled");
+    $("#specialMessage").text("");
+}
+
+function replaceDiacritics(cityStateCountry) {
+    var diacriticsArray = /[ÀÁÂÃÄÅàáâãäåÈÉÊËèéêëÌÍÎÏìíîïÒÓÔÕÖØòóôõöøÙÚÛÜùúûü]/;
+    var diacriticExists = $.each(diacriticsArray, function (i, diacritic) {
+        var retVal = false;
+        if (cityStateCountry.indexOf(diacritic) > -1) {
+            retVal = true;
+            return false;//breaks loop, doesn't return false from the function
+        }
+        return retVal;
+    });
+
+    if (diacriticExists) {
+        cityStateCountry = cityStateCountry.replace(/[ÀÁÂÃÄÅ]/, "A");
+        cityStateCountry = cityStateCountry.replace(/[àáâãäå]/, "a");
+        cityStateCountry = cityStateCountry.replace(/[ÈÉÊË]/, "E");
+        cityStateCountry = cityStateCountry.replace(/[èéêë]/, "e");
+        cityStateCountry = cityStateCountry.replace(/[ÌÍÎÏ]/, "I");
+        cityStateCountry = cityStateCountry.replace(/[ìíîï]/, "i");
+        cityStateCountry = cityStateCountry.replace(/[ÒÓÔÕÖØ]/, "O");
+        cityStateCountry = cityStateCountry.replace(/[òóôõöø]/, "o");
+        cityStateCountry = cityStateCountry.replace(/[ÙÚÛÜ]/, "U");
+        cityStateCountry = cityStateCountry.replace(/[ùúûü]/, "u");
+    }
+    return cityStateCountry;
+}
+
+function getCityStateCountryFromAddressComponents(addressComponents) {
+    // iterate through address_component array and pull out city, state country
+    var city = "";
+    var state = "";
+    var country = "";
+    $.each(addressComponents, function (i, address_component) {
+
+        if (address_component.types[0] == "country") {
+            country = address_component.long_name.replace(/ /g, "_");;
+        }
+        if (address_component.types[0] == "locality") {
+            city = address_component.long_name.replace(/ /g, "_");
+        }
+        if (address_component.types[0] == "administrative_area_level_1") {
+            state = address_component.long_name.replace(/ /g, "_");;
+        }
+    });
+    return city + "|" + state + "|" + country;
+}
+
+// onError Callback receives a PositionError object
+//
+function onGeolocationError(error) {
+    toggleWaitAnim(false);
+    var message = "";
+    // Check for known errors
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            message = "This website does not have permission to use " +
+                      "the Geolocation API";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            message = "The current position could not be determined. " +
+                    "Check your internet connection";
+            break;
+        case error.POSITION_DENIED_TIMEOUT:
+            message = "The current position could not be determined " +
+                      "within the specified timeout period.";
+            break;
+        case error.TIMEOUT:
+            message = "The current position could not be determined " +
+                      "within the specified timeout period.";
+            break;
+    }
+    if (message != "") {
+        alert('Can\'t get latitude and longitude information from this device - but you can still enter a city or ZIP code. \n\n' + message);
+        $("#btnUsePhoneLatLon").hide();
+        $("#txtAddress").focus();
+        return;
+    }
+}
+
+function friendlyGeocodeErrorMessage(errorCode) {
+    switch (errorCode) {//message string variables are from localization.js
+        case google.maps.GeocoderStatus.ZERO_RESULTS:
+            return geocodeZeroResults;
+            break;
+        case google.maps.GeocoderStatus.OVER_QUERY_LIMIT:
+            return geocodeOverQueryLimit;
+            break;
+        case google.maps.GeocoderStatus.REQUEST_DENIED:
+            return geocodeRequestDenied;
+            break;
+        case google.maps.GeocoderStatus.INVALID_REQUEST:
+            return geocodeInvalidRequest;
+            break;
+        default:
+            return geocodeGeneric + errorCode;
+    }
+}
+
+function friendlyNasaErrorMessage(errorCode) {
+    toggleWaitAnim(false);
+    return (errorCode);
+}
+
+// onSuccess Geolocation
+//
+function onGeolocationSuccess(position) {
+    $("#lat").val(position.coords.latitude);
+    $("#lng").val(position.coords.longitude); 
+    $("#btnUsePhoneLatLon").show();
+}
+
+function getCurrentPositionError(error) {
+    //$("#enableLocation").show();
+}
+
+/*********************************HTML5 Local Storage code***********************************/
+function addToPreviousLocations(address) {
+    var storedLocations = localStorage.getItem("storedLocations"); //returns triple pipe (|||) delimited string.
+    if (storedLocations == null || !locationAlreadyExistsInLocalStorage(address, storedLocations)) {
+        storedLocations = address.replace('|', '') + '|||' + storedLocations;
+        storedLocations = trimToFiveLocations(storedLocations);
+        localStorage.setItem("storedLocations", storedLocations);
+    }
+}
+
+function trimToFiveLocations(storedLocations) {
+    var storedLocationArray = storedLocations.split('|||');
+    var len = storedLocationArray.length - 1;
+    if (len > 5) {
+        storedLocationArray = storedLocationArray.slice(0, len);
+        return storedLocationArray.join("|||");
+    }
+    return storedLocations;
+}
+
+function locationAlreadyExistsInLocalStorage(address, storageString) {
+    return (storageString.indexOf(address) > -1);
+}
+
+function getPreviousLocationsFromLocalStorage() {
+    //if (Modernizr.localstorage) {
+        $(".storedLocation").remove();
+        var storedLocations = localStorage.getItem("storedLocations") != null ? localStorage.getItem("storedLocations").split('|||') : "";
+        var upToMax = storedLocations.length > 5 ? 5 : storedLocations.length;
+        for (var counter = 0; counter < upToMax; counter++) {
+            if (counter == 0) { $("#txtAddress").val(storedLocations[0]) };
+            $("#storedLocations").append("<div class='storedLocation'>" + storedLocations[counter] + "</div>");
+        }
+        $(".storedLocation").click(function () { $("#txtAddress").val($(this).text()); getLatLonViaGoogle(); });
+    //}
+}
+
+function removeStoredLocations() {
+    if (Modernizr.localstorage) {
+        if (confirm(doYouReallyWantToRemoveLocalizedText)) {//from localization.js
+            localStorage.setItem("storedLocations", "");
+            $(".storedLocation").remove();
+        }
+    }
+}
+/*********************************End HTML5 Local Storage code***********************************/
+/***************************************** Wait Animation code **************************************/
+
+var waitAnimHandle = "";
+var waitImage
+function toggleWaitAnim(start) {
+    waitImage = $("#waitAnim");
+    if (start) {
+        if (waitAnimHandle == "") {//don't start an anim if one's already running.
+            var divElement = document.createElement('div');
+            divElement.setAttribute("id", "searchingText");
+            divElement.textContent = "Searching..."
+            $("#waitAnimContainer").append(divElement);
+            $("#waitAnimContainer").css("opacity", "1.0");
+            waitAnimHandle = setInterval(waitAnimIncrementFrame, 100, false);
+        }
+    }
+    else {
+        clearInterval(waitAnimHandle);
+        waitAnimHandle = "";
+        $("#searchingText").remove();
+        $("#waitAnimContainer").css("opacity", "0.1");
+    }
+}
+
+var currAnimFrame = 1;
+var incr = true;
+function waitAnimIncrementFrame() {
+    incr ? currAnimFrame++ : currAnimFrame--;
+    if (currAnimFrame == 1) { incr = true; }
+    else if (currAnimFrame == 4) { incr = false; };
+    waitImage.attr('src', 'images/Sputnik0' + currAnimFrame + '.png')
+}
+
+
+/*****************************************End  Wait Animation code **************************************/
+
+
+/***************************************** Begin Alert code **************************************/
+
+
+    // snooze...
+//window.wakeuptimer.snooze(successCallback,
+//    errorCallback,
+//    {
+//        alarms: [{
+//            type: 'snooze',
+//            time: { seconds: 60 }, // snooze for 60 seconds 
+//            extra: {'Waking up!!!'}, // json containing app-specific information to be posted when alarm triggers
+//            message: this.get('message'),
+//            sound: this.get('sound'),
+//            action: this.get('action')
+//        }]
+//    }
+// );
+
+    // example of a callback method
+var alertSuccessCallback = function (result) {
+    if (result.type && result.type === 'wakeup') {
+        navigator.notification.alert(JSON.parse(result.extra).message, null, 'ISS Alert:', 'Got it');
+        navigator.notification.beep(4);//beep 3 times;
+        navigator.vibrate([500, 500, 500, 500, 1000, 1000, 3000, 500, 500]);//vibration pattern - milliseconds to alternate vibration/pause.
+    } else if (result && result.type === 'set') {
+        console.log('ISS Sighting alarm set for ' + new Date(result.alarm_date));
+        navigator.notification.alert('ISS Sighting alarm set for ' + new Date(result.alarm_date), null, 'Alert set', 'OK');
+    } else {
+        navigator.notification.alert('ISS Sighting alarm has been set', null, 'Alert set', 'OK');
+    }
+};
+
+var alertErrorCallback = function (result) {
+    alert("error");
+}
+
+
+})();
---
+++ SpaceStationFinder/www/scripts/localization.js
@@ -0,0 +1,122 @@
+﻿
+var baseLang = "en";
+var btnGetLatLonLocalizedText = "SHOW VIEW";
+var btnUseCurrentLocationLocalizedText = "JUST USE MY CURRENT LOCATION";
+var lblLocationNotSetLocalizedText = "Where on earth do you want to view the Space Station?";
+var lblZipLocalizedText = "Nearest City";
+var storedLocationsLocalizedText = "Your Locations:";
+var clearLocationsLocalizedText = "(REMOVE ALL)";
+var getNewerViewingInfo = "Get newer viewing info?";
+var thisViewingIsOld = "This viewing is old";
+var resumeWindowButtons = "Yes, No - Let me see the previous view";
+
+//Messages:
+var doYouReallyWantToRemoveLocalizedText = "Do you really want to delete all stored display locations?";
+var geocodeZeroResults = "Location not found. doublecheck spelling? Try a larger city?";
+var geocodeInvalidRequest = "We could not determine your location. The error returned usually means either no ZIP was entered, or the zip or address was not valid.";
+var geocodeOverQueryLimit = "We are over our quota of calls to the Google location service. Try again later.";
+var geocodeRequestDenied = "Our attempt to determine your location was denied for some reason.";
+var geocodeGeneric = "An error occurred, but the location finder did not tell us what is wrong. Make sure you are connected to the internet: ";
+var uhaapiNoSatellite = "Trouble retrieving pass info for that location: \nThe error code indicates a specific address or zip code may be required. \n\nSorry for the additional keystrokes.";
+var enableLocation = "For one-click viewing, you should<br /><strong style='font-size:14px;color:#449'>Enable Location</strong><br /> (under Android <em>Settings</em>)<br />(You won't have to type  in a city any more!)";
+
+var specialMessageLocalizedText = "There are very few viewing opportunities"
+var noteLocalizedText = "NOTE: ";
+
+if (navigator.userLanguage)//IE specific
+    baseLang = navigator.userLanguage.substring(0, 2).toLowerCase();
+else
+    baseLang = navigator.language.substring(0, 2).toLowerCase();
+
+switch (baseLang) {
+    case "it": //Italian
+        doYouReallyWantToRemoveLocalizedText = "Sei sicuro di voler eliminare tutte le località di visualizzazione memorizzati?";
+        btnGetLatLonLocalizedText = "MOSTRA VISTA";
+        btnUseCurrentLocationLocalizedText = "USA MIA POZIZIONE CORRENTE";
+        lblLocationNotSetLocalizedText = "Dove diavolo si desidera visualizzare la Stazione Spaziale?";
+        lblZipLocalizedText = "città";
+        storedLocationsLocalizedText = "Delle posizioni:";
+        clearLocationsLocalizedText = " (RIMUOVERE TUTTI)";
+        getNewerViewingInfo = "Prendi recente Visualizzazione Info?";
+        thisViewingIsOld = "Questa visione è vecchio";
+        resumeWindowButtons = "Sì, No - Fammi vedere la vista precedente";
+        //Messages
+        geocodeZeroResults = "Il geocode era successo, ma non ha prodotto risultati. Ciò può verificarsi se un ZIP inesistente o non valido o l'indirizzo è stato inserito. Prova di nuovo. (forse alcuni caratteri non alfanumerici stanno causando un problema)";
+        geocodeInvalidRequest = "Non è stato possibile determinare la propria posizione. L'errore restituito di solito significa sia nessuna città è stato inserito o la città non era valido.";
+        geocodeOverQueryLimit = "Siamo sopra la nostra quota di chiamate al servizio di localizzazione di Google. Riprovare più tardi.";
+        geocodeRequestDenied = "Il nostro tentativo di determinare la posizione è stato negato.";
+        geocodeGeneric = "È verificato un errore, ma il cercatore di posizione non ci ha detto ciò che è sbagliato. Assicurarsi di essere connessi a Internet:";
+        uhaapiNoSatellite = "Stiamo avendo problemi recuperando informazioni pass per quella posizione: \nIl codice di errore indica un indirizzo specifico o il codice postale può essere richiesto. \n\nSorry per le combinazioni di tasti.";
+        enableLocation = "Abilita Location (in Impostazioni) per un clic di visualizzazione. (Non dovrete digitare un nome di città più)";
+        break;
+    case "nl": //Dutch - Netherlands
+        doYouReallyWantToRemoveLocalizedText = "Bent u echt willen schrappen alle opgeslagen aanwijsplaat plaatsen?";
+        btnGetLatLonLocalizedText = "Weergeven";
+        btnUseCurrentLocationLocalizedText = "Gebruik mijn huidige locatie";
+        lblLocationNotSetLocalizedText = "Waar ter wereld wilt u het ruimtestation te bekijken?";
+        lblZipLocalizedText = "City";
+        storedLocationsLocalizedText = "Uw Locaties:";
+        clearLocationsLocalizedText = " (VERWIJDER ALLE)";
+        getNewerViewingInfo = "Krijgen Nieuwer Bekijkt Info?";
+        thisViewingIsOld = "Deze weergave is oud";
+        resumeWindowButtons = "Ja, Nee - Laat me de vorige weergave";
+        //Messages
+        geocodeZeroResults = "De Geocode was succesvol, maar heeft geen resultaten opgeleverd. Dit kan gebeuren als een niet-bestaand of ongeldig ZIP of het adres is ingevoerd. proberen weer. (misschien een aantal niet-alfanumerieke tekens veroorzaken een probleem)";
+        geocodeInvalidRequest = "We konden niet het bepalen van uw locatie. De fout betekent meestal dat het adres niet geldig was.";
+        geocodeOverQueryLimit = "Wij zijn over onze quotum van oproepen naar de Google-locatie dienst. Probeer het later opnieuw.";
+        geocodeRequestDenied = "Onze poging om je locatie te bepalen werd geweigerd om een ​​bepaalde reden.";
+        geocodeGeneric = "Er is een fout opgetreden, Zorg ervoor dat u verbonden bent met het internet:";
+        uhaapiNoSatellite = "Problemen ophalen van pas info voor die locatie: \nDe error code geeft een specifiek adres of postcode nodig zijn. \n \nSorry voor de extra toetsaanslagen.";
+        enableLocation = "Inschakelen Locatie (onder Instellingen) voor een-klik bekijken. (U hoeft niet een naam van een stad niet meer te typen)";
+        break;
+    case "es": //Spanish
+        doYouReallyWantToRemoveLocalizedText = "¿Realmente desea eliminar todas las ubicaciones de visualización almacenada?";
+        btnGetLatLonLocalizedText = "Vista desde esta ubicación";
+        btnUseCurrentLocationLocalizedText = "Utilizar mi ubicación actual";
+        lblLocationNotSetLocalizedText = "Donde en la tierra usted desea ver?";
+        lblZipLocalizedText = "Ciudad";
+        storedLocationsLocalizedText = "Ubicaciones almacenados";
+        clearLocationsLocalizedText = " (ELIMINAR TODOS)";
+        getNewerViewingInfo = "Obtén más reciente Viendo informaciones?";
+        thisViewingIsOld = "Esta visión es viejo";
+        resumeWindowButtons = "Sí, No - Déjame ver la vista previa";
+        //Messages
+        geocodeZeroResults = "La codificación geográfica fue un éxito, pero no tuvo ningún resultado. Esto puede ocurrir si una postal no existe o es inválida o la dirección se ha introducido. Inténtelo de nuevo. (tal vez algunos caracteres no alfanuméricos están causando un problema)";
+        geocodeInvalidRequest = "No se pudo determinar su ubicación. El error suele significar o bien no se ha introducido ZIP, o el código postal o la dirección no era válida.";
+        geocodeOverQueryLimit = "Estamos en nuestra cuota de llamadas al servicio de localización de Google. Inténtelo más tarde.";
+        geocodeRequestDenied = "Nuestro intento de determinar su ubicación se le negó por alguna razón.";
+        geocodeGeneric = "Ha ocurrido un error, pero el buscador de ubicaciones no nos dicen lo que está mal. Asegúrese de que está conectado a Internet:";
+        uhaapiNoSatellite = "Problemas para recuperar información pasar por ese lugar: \nEl código de error indica una dirección específica o código postal puede ser requerida. \n\nSorry de las pulsaciones de teclas adicionales.";
+        enableLocation = "Activar Ubicación (en Configuración) para un solo clic de visualización. (Usted no tiene que escribir un nombre de ciudad más)"
+        break;
+    case "ru": //Russian
+        doYouReallyWantToRemoveLocalizedText = "Вы действительно хотите удалить все сохраненные местах экрана?";
+        btnGetLatLonLocalizedText = "Вид с этого места";
+        btnUseCurrentLocationLocalizedText = "Использовать мое настоящее место";
+        lblLocationNotSetLocalizedText = "Где же ты хочешь, чтобы посмотреть откуда?";
+        lblZipLocalizedText = "город";
+        storedLocationsLocalizedText = "Хранимые проведения";
+        clearLocationsLocalizedText = " (Удалить все)";
+        enableLocation = "Включить Location (в меню Настройки) для просмотра один клик. (Вам не придется вводить название города больше)";
+        getNewerViewingInfo = "Получить Новее Просмотр информация?";
+        thisViewingIsOld = "Этот визуальный осмотр стар";
+        resumeWindowButtons = "Да, нет - Позвольте мне см. предыдущий вид";
+        //Messages
+        geocodeZeroResults = "Геокодирование прошла успешно, но не дал результатов. Это может произойти, если несуществующего или недействительного ZIP или адрес был введен. Попробуйте еще раз. (может быть, некоторые не алфавитно-цифровые символы вызывают проблемы)";
+        geocodeInvalidRequest = "Мы не могли определить ваше местоположение.Ошибка обычно означает, адрес не был в силе.";
+        geocodeOverQueryLimit = "Мы над нашей квоте звонков на расположение службы Google. Попробуйте еще ​​раз позже.";
+        geocodeRequestDenied = "Наша попытка определить ваше местоположение было отказано по некоторым причинам.";
+        geocodeGeneric = "Произошла ошибка, но расположение видоискателя не сказал нам, что это неправильно. Убедитесь, что вы подключены к Интернету:";
+        uhaapiNoSatellite = "Проблема получения информации пройти для этого места:\nВ код ошибки указывает на определенный адрес или почтовый индекс может оказаться необходимым.\п\nSorry для дополнительных клавиш.";
+        break;
+}
+
+function setLocalizedText() {
+    $("#btnGetLatLon").attr("value", btnGetLatLonLocalizedText);
+    $("#btnUsePhoneLatLon").text(btnUseCurrentLocationLocalizedText);
+    $("#lblLocationNotSet").text(lblLocationNotSetLocalizedText);
+    $("#lblZip").text(lblZipLocalizedText);
+    $("#lblStoredLocations").text(storedLocationsLocalizedText);
+    $("#lblClearLocations").text(clearLocationsLocalizedText);
+    $("#enableLocation").html(enableLocation);
+}
